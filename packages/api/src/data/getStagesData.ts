4e020c9df2d9e237801879f99f8edf9c96b3c540
André Martins <tecnologia.ri1anapolis@gmail.com>
2021-11-30 09:59:27 -0300

    Remove unnecessary data from sql query

--- packages/api/src/data/getStagesData.ts
+++ packages/api/src/data/getStagesData.ts
@@ -16,7 +16,7 @@ const getStageData = async () => {
     FROM sqlreg3.l1
     INNER JOIN sqlreg3.etapa AS et ON(l1.l1_etapa = et.Et_id)
     LEFT JOIN sqlreg3.l1_indicadoretapas AS l1et ON(l1.L1_Protocolo = l1et.ie1_protocolo AND et.Et_id = l1et.ie1_etapa AND l1et.ie1_dataentrada >= l1.L1_DataEtapa)
-    WHERE l1.L1_Ativo = 1 AND l1.l1_outro = 0 AND l1.l1_duvida = 0
+    WHERE l1.L1_Ativo = 1 AND l1.l1_outro = 0 AND l1.l1_duvida = 0 AND et.Et_id NOT IN (15, 22)
     ORDER BY et.et_ordem, l1.L1_Protocolo, l1.L1_Natureza
   `)
 
